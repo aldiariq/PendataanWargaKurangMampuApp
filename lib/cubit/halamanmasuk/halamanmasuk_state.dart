43a424e20a54cd9f95fec0d6e1f2d1945525a694
aldiariq <aldi48767@gmail.com>
2021-08-30 08:48:44 +0700

    adding halaman masuk screen and route

---
+++ lib/cubit/halamanmasuk/halamanmasuk_state.dart
@@ -0,0 +1,6 @@
+part of 'halamanmasuk_cubit.dart';
+
+@immutable
+abstract class HalamanmasukState {}
+
+class HalamanmasukInitial extends HalamanmasukState {}
--- lib/route/routeApp.dart
+++ lib/route/routeApp.dart
@@ -1,16 +1,20 @@
 import 'package:apppendataanwargakurangmampu/constants/strings.dart';
+import 'package:apppendataanwargakurangmampu/presentation/halaman_masuk.dart';
 import 'package:apppendataanwargakurangmampu/presentation/halaman_splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:apppendataanwargakurangmampu/cubit/splashscreen/splashscreen_cubit.dart';
+import 'package:apppendataanwargakurangmampu/cubit/halamanmasuk/halamanmasuk_cubit.dart';
 
 class RouteApp {
 
   SplashscreenCubit splashscreenCubit;
+  HalamanmasukCubit halamanMasukCubit;
 
   RouteApp(){
     splashscreenCubit = SplashscreenCubit();
+    halamanMasukCubit = HalamanmasukCubit();
   }
 
   Route generateRoute(RouteSettings routeSettings){
@@ -21,7 +25,10 @@ class RouteApp {
           child: HalamanSplashscreen(),
         ));
       case HALAMAN_MASUK:
-        return null;
+        return MaterialPageRoute(builder: (_) => BlocProvider.value(
+          value: halamanMasukCubit,
+          child: HalamanMasuk(),
+        ));
       case HALAMAN_DASHBOARD:
         return null;
       case HALAMAN_RUKUN_TETANGGA:
